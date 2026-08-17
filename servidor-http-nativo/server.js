d6187c476e9ed637e95db8f3b6f133cc349713e4
mariaelo-hub <mariaeloizaramos97@gmail.com>
2026-08-17 20:52:30 +0000

    criou as rotas

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,16 +1,32 @@
-const http = require ('node:http');
-const PORTA = 3000;
+ import http from 'node:http'
+import { URL } from 'node:url'
+
+const porta = 3000
+
+const produtos = [
+    {id: 1, nome: "Sabonete"},
+    {id: 2, nome: "Volante LogiTech G923"},
+    {id: 3, nome: "Sabão em Pó"},
+    {id: 4, nome: "Pelúcia do Sonic"},
+]
+
 const server = http.createServer((req, res) => {
-  console.log(`Requisição recebida! ${req.method} ${req.url}`);
-  
-  res.statusCode = 201;
-  res.setHeader ('Content-Type', 'application/json; charset=utf-8');
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
 
- (JSON.stringify({ status: "ok" }));
+    if (req.method == "GET" && req.url == "/contato") {
+        return res.end(JSON.stringify({data:
+            {numero_telefone: "67 99999 9999",
+                endereco: "Rua da Alegria, 99, Centro"}}));
+    }
+
+    if (req.method == "GET" && req.url == "/produtos") {
+        return res.end(JSON.stringify(produtos));
+    }
+
+    res.end(JSON.stringify({data: "Página Inicial"}))
+})
+
+server.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`)
 });
- server.listen(PORTA, () => {
-  console.log(new Date().toISOString());
-  console.log(`Servidor funcionando na porta ${PORTA}`);
- });
-
- // O código não exibiria a mensagem sem o res.end 
