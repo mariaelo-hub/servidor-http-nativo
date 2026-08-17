812f044f70321aedeb32750183552b31d4aa9be7
mariaelo-hub <mariaeloizaramos97@gmail.com>
2026-08-17 21:38:39 +0000

    status modificado

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -26,7 +26,10 @@ const server = http.createServer((req, res) => {
     if (req.method == "GET" && req.url == "/produtos") {
         return res.end(JSON.stringify(produtos));
     }
-
+    else{
+        res.statusCode = 404
+          return res.end(JSON.stringify("error 404, nao encontrada"));
+    }
 
     res.end(JSON.stringify({data: "Página Inicial"}))
 })
